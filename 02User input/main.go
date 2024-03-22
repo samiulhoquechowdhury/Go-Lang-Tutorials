3fa1de1c3a3369ce5e72ff7f084dbddbf18b258d
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-03-23 00:16:19 +0530

    first commit

--- 02User input/main.go
+++ 02User input/main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	welcome := "Welcome to the course"
+	welcome := "Welcome to user input"
 	fmt.Println(welcome)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating for the Hotel:")
+
+	//comma ok // err ok
+
+	input, _ := reader.ReadString('\n')
+
+	fmt.Println("Thank you for rating, ", input)
+	fmt.Printf("Type of this rating is %T", input)
+
 }
